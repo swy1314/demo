23d63c2fa4fc885d04fa08b8b5b53334a32fdf29
swy <xjj314@163.com>
2017-08-09 09:10:02 +0800

    创建项目

---
+++ src/main/java/com/swy/DemoDockerApplication.java
@@ -0,0 +1,12 @@
+package com.swy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDockerApplication.class, args);
+	}
+}
